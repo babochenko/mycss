7929b256d320a0d2965516d72e8d5d8947dabd2a
denisbabochenko <babochenko.denis2@gmail.com>
2025-06-15 12:26:40 +0100

    hide right col, only if no playlist

--- youtube.js
+++ youtube.js
@@ -1,4 +1,5 @@
 const $ = (sel) => document.querySelector(sel);
+const $$ = (sel) => document.querySelectorAll(sel);
 
 const onKey = (key, lambda) => {
   onKeyOn(document, key, lambda);
@@ -39,4 +40,17 @@ onKey('c', () => {
   chatInputObserver.disconnect()
   chatInputObserver.observe(document.body, { childList: true, subtree: true });
 });
+
+// hide video suggestions, if no playlist chapters
+const rightColObserver = new MutationObserver(() => {
+  const rightCol = $('#secondary');
+
+  if (rightCol) {
+    rightColObserver.disconnect()
+    if ($$('#playlist > #container > #items > *').length == 0) {
+      rightCol.style.display = 'none';
+    }
+  }
+});
+rightColObserver.observe(document.body, { childList: true, subtree: true });
 
